c32243696118d63e1d3a98fb4e796b1ad54fce50
Patrick Bogen <pdbogen-github@cernu.us>
2015-04-30 23:09:53 -0700

    union for Payload, with some constructors; simple ring-buffer-based packet queue; some packet constructors and a debug-print.  the first few command assignments. packet queue implementation.

--- common/Packet/Packet.cpp
+++ common/Packet/Packet.cpp
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <util/crc16.h>
+#include <string.h>
 
 void Packet::sign() {
 	crc = 0;
@@ -18,4 +19,63 @@ uint8_t Packet::check() {
 		crc_calc = _crc16_update( crc_calc, payload.bytes[i] );
 		//((uint8_t*)(payload))[i] );
 	return( crc_calc == crc );
+}
+
+PacketQueue::PacketQueue() {
+	_front = 0;
+	_back = 0;
+	time_ms = 0;
+	wait_ms = 100;
+}
+
+const Packet * PacketQueue::front() {
+	if( _front == _back )
+		return 0;
+	return &(queue[_front]);
+}
+
+uint8_t PacketQueue::enqueue( uint8_t command ) {
+	enqueue( command, Payload() );
+}
+
+uint8_t PacketQueue::enqueue( uint8_t command, Payload payload ) {
+	if( (_back+1) % PACKET_QUEUE_SIZE != _front ) {
+		queue[_back].command = command;
+		queue[_back].payload = payload;
+		queue[_back].sign();
+		_back = (_back+1) % PACKET_QUEUE_SIZE;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+uint8_t PacketQueue::enqueue( Packet * entrant ) {
+	if( (_back+1) % PACKET_QUEUE_SIZE != _front ) {
+		memcpy( &(queue[_back]), entrant, sizeof( Packet ) );
+		_back = (_back+1) % PACKET_QUEUE_SIZE;
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+void PacketQueue::dequeue() {
+	if( _front == _back )
+		return;
+	_front = (_front + 1) % PACKET_QUEUE_SIZE;
+	time_ms = 0;
+	wait_ms = 100;
+}
+
+void Packet::print( HardwareSerial s ) const {
+	char buf[64];
+	sprintf( buf, "%02x | ", command );
+	s.print( buf );
+	for( uint8_t i = 0; i < sizeof( Payload ); i++ ) {
+		sprintf( buf, "%02x ", payload.bytes[i] );
+		s.print( buf );
+	}
+	sprintf( buf, "| %02x %02x", ((uint8_t*)(&crc))[0], ((uint8_t*)(&crc))[1] );
+	s.print( buf );
 }
--- common/Packet/Packet.h
+++ common/Packet/Packet.h
@@ -1,22 +1,60 @@
 #ifndef PACKET_H
 #define PACKET_H
 
-#define CMD_ACK 0
+#ifndef PACKET_QUEUE_SIZE
+#define PACKET_QUEUE_SIZE 10
+#endif
+
+#define CMD_ACK  0
+#define CMD_PING 1
+#define CMD_REPORT_TEMP 2
+#define CMD_SET_TEMP_0 3
 
 #include <stdint.h>
 
+#include <HardwareSerial.h>
+
+union Payload {
+	long integer;
+	unsigned long uinteger;
+	float decimal;
+	uint16_t crc;
+	const uint8_t bytes[];
+	Payload() {}
+	Payload( long integer ) : integer( integer ) { }
+	Payload( unsigned long uinteger ) : uinteger( uinteger ) { }
+	Payload( float decimal ) : decimal( decimal ) { }
+	Payload( uint16_t crc ) : crc( crc ) { }
+	Payload( const Payload &p ) : uinteger( p.uinteger ) { }
+	Payload& operator=(const Payload &p) { uinteger = p.uinteger; return *this; }
+};
+
 struct Packet {
 	uint8_t command;
-	union {
-		long integer;
-		unsigned long uinteger;
-		float decimal;
-		uint16_t crc;
-		uint8_t  bytes[];
-	} payload;
+	Payload payload;
 	uint16_t crc;
 	void sign();
 	uint8_t check();
+	Packet() {}
+	Packet( uint8_t command ) : command( command ) { sign(); }
+	Packet( uint8_t command, Payload payload ) : command( command ), payload( payload ) { sign(); }
+	void print( HardwareSerial s ) const;
+};
+
+// Simple ring-buffer-based queue
+class PacketQueue {
+private:
+	Packet queue[PACKET_QUEUE_SIZE];
+	uint8_t _front, _back;
+public:
+	PacketQueue();
+	const Packet * front();
+	void dequeue();
+	uint8_t enqueue( Packet * );
+	uint8_t enqueue( uint8_t command );
+	uint8_t enqueue( uint8_t command, Payload payload );
+	unsigned long time_ms;
+	uint16_t wait_ms;
 };
 
 #endif // PACKET_H

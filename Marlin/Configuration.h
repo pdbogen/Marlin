b85cbd29010611599833c3551a6d4653025018ca
Patrick Bogen <pdbogen-github@cernu.us>
2015-04-30 23:10:53 -0700

    Two extruders, one of which is local

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -88,7 +88,8 @@ Here are some standard links for getting your machine calibrated:
 
 // This defines the number of extruders
 // :[1,2,3,4]
-#define EXTRUDERS 1
+#define EXTRUDERS 2
+#define LOCAL_EXTRUDERS 1
 
 // Offset of the extruders (uncomment if using more than one and relying on firmware to position when changing).
 // The offset has to be X=0, Y=0 for the extruder 0 hotend (default extruder).

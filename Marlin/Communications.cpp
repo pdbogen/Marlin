4422e1e1da907bbdc43f0bb9d609d5e39e57b97a
Patrick Bogen <pdbogen-github@cernu.us>
2015-04-30 23:10:12 -0700

    Make these symlinks to the slave comms code

--- Marlin/Communications.cpp
+++ Marlin/Communications.cpp
@@ -1,6 +1,1 @@
-#include "Communications.h"
-
-void setup_slave_communications() {
-	SLAVE_SERIAL.begin( SLAVE_BAUDRATE );
-}
-
+../Slave/Communications.cpp
--- Marlin/Communications.h
+++ Marlin/Communications.h
@@ -1,10 +1,1 @@
-#ifndef COMMUNICATIONS_H
-#define COMMUNICATIONS_H
-
-#include "Configuration.h"
-
-#include <Packet.h>
-
-void setup_slave_communications();
-
-#endif // COMMUNICATIONS_H
+../Slave/Communications.h

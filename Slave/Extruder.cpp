5499bdd0f238614894c38a1574ec3c1273e8413f
Patrick Bogen <pdbogen-github@cernu.us>
2015-04-30 23:06:12 -0700

    Temperature setting and PID control

--- Slave/Extruder.cpp
+++ Slave/Extruder.cpp
@@ -13,3 +13,39 @@ float Extruder::getTemperature() {
 void Extruder::calculateRInf() {
 	RInf = NTC * exp( -beta / 298.15 );
 }
+
+void Extruder::runPID() {
+	unsigned long dt = millis() - last;
+	if( dt < PID_INTERVAL )
+		return;
+
+	float error = target_temperature - temperature;
+	if( error > 10 || error < -10 ) {
+		integral = 0;
+	} else {
+		integral = integral + error * dt;
+		if( integral > integral_max )
+			integral = integral_max;
+		if( integral < -integral_max )
+			integral = -integral_max;
+	}
+	float derivative = (error - prev_error) / dt;
+	prev_error = error;
+	float output = error * kp + integral * ki + derivative * kd;
+	#ifdef DEBUG_PID
+		DEBUG_IO.print( "measurement=" ); DEBUG_IO.print( temperature ); DEBUG_IO.print( " setpoint=" ); DEBUG_IO.println( target_temperature );
+		DEBUG_IO.print( "P=" ); DEBUG_IO.print( error ); DEBUG_IO.print( " I=" ); DEBUG_IO.print( integral ); DEBUG_IO.print( "D=" ); DEBUG_IO.println( derivative );
+		DEBUG_IO.print( "output=" ); DEBUG_IO.print( output );
+	#endif
+	if( output < 0 ) {
+		digitalWrite( heater_pin, LOW );
+		DEBUG_IO.println( " set=LOW" );
+	} else if( output >= 255 ) {
+		digitalWrite( heater_pin, HIGH );
+		DEBUG_IO.println( " set=HIGH" );
+	} else {
+		analogWrite( heater_pin, output );
+		DEBUG_IO.print( " set=" ); DEBUG_IO.println( (uint8_t) output );
+	}
+	last = dt + last;
+}
--- Slave/Extruder.h
+++ Slave/Extruder.h
@@ -1,11 +1,16 @@
 #ifndef EXTRUDER_H
 #define EXTRUDER_H
 
+#include "Configuration.h"
+
+#include <Arduino.h>
 #include <stdint.h>
 
 struct Extruder {
+	float target_temperature;
 	float temperature;
 	float beta;
+	float integral_max;
 	unsigned long Rs;
 	float RInf;
 	uint8_t temp_pin;
@@ -15,9 +20,23 @@ struct Extruder {
 		beta = 0;
 		Rs = 0;
 		RInf = 0;
+		integral_max = 1000.0 * PID_INTERVAL;
+		kp = 20; ki = 0.3 / PID_INTERVAL; kd = 10;
+		integral = 0; prev_error = 0; last=millis();
 	}
 	float getTemperature();
 	void calculateRInf();
+	void setTemperature( float celsius ) { target_temperature = celsius; integral = 0; prev_error = 0; }
+	void runPID();
+	void setHeaterPin( uint8_t p ) { pinMode( p, OUTPUT ); heater_pin = p; }
+
+private:
+	uint8_t kp, kd;
+	float ki;
+	float prev_error;
+	float integral;
+	unsigned long last;
+	uint8_t heater_pin;
 };
 
 #endif // EXTRUDER_H

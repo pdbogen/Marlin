ca192db2136a9a3e61a5033a29f530c39282fd7a
Patrick Bogen <pdbogen-github@cernu.us>
2015-05-01 17:32:18 -0700

    Map enable_e# and disable_e# to slave_set commands according to number of local extruders

--- Marlin/Marlin.h
+++ Marlin/Marlin.h
@@ -160,24 +160,39 @@ void manage_inactivity(bool ignore_stepper_queue = false);
 #endif
 
 #if (EXTRUDERS > 1) && HAS_E1_ENABLE
-  #define enable_e1()  E1_ENABLE_WRITE( E_ENABLE_ON)
-  #define disable_e1() E1_ENABLE_WRITE(!E_ENABLE_ON)
+  #if LOCAL_EXTRUDERS > 1
+    #define enable_e1()  E1_ENABLE_WRITE( E_ENABLE_ON)
+    #define disable_e1() E1_ENABLE_WRITE(!E_ENABLE_ON)
+  #else
+    #define enable_e1() slave_set_extruder_enabled(0);
+    #define disable_e1() slave_set_extruder_disabled(0);
+  #endif
 #else
   #define enable_e1()  /* nothing */
   #define disable_e1() /* nothing */
 #endif
 
 #if (EXTRUDERS > 2) && HAS_E2_ENABLE
-  #define enable_e2()  E2_ENABLE_WRITE( E_ENABLE_ON)
-  #define disable_e2() E2_ENABLE_WRITE(!E_ENABLE_ON)
+  #if LOCAL_EXTRUDERS > 2
+    #define enable_e2()  E2_ENABLE_WRITE( E_ENABLE_ON)
+    #define disable_e2() E2_ENABLE_WRITE(!E_ENABLE_ON)
+  #else
+    #define enable_e2() slave_set_extruder_enabled(1);
+    #define disable_e2() slave_set_extruder_disabled(1);
+  #endif
 #else
   #define enable_e2()  /* nothing */
   #define disable_e2() /* nothing */
 #endif
 
 #if (EXTRUDERS > 3) && HAS_E3_ENABLE
-  #define enable_e3()  E3_ENABLE_WRITE( E_ENABLE_ON)
-  #define disable_e3() E3_ENABLE_WRITE(!E_ENABLE_ON)
+  #if LOCAL_EXTRUDERS > 3
+    #define enable_e3()  E3_ENABLE_WRITE( E_ENABLE_ON)
+    #define disable_e3() E3_ENABLE_WRITE(!E_ENABLE_ON)
+  #else
+    #define enable_e3() slave_set_extruder_enabled(2);
+    #define disable_e3() slave_set_extruder_disabled(2);
+  #endif
 #else
   #define enable_e3()  /* nothing */
   #define disable_e3() /* nothing */

040ae69b3c42711b015257fa286db842e7a65163
Patrick Bogen <pdbogen-github@cernu.us>
2015-05-01 17:30:59 -0700

    Add list of enable pins

--- Slave/Globals.cpp
+++ Slave/Globals.cpp
@@ -5,8 +5,9 @@
 
 Extruder extruders[HOT_ENDS];
 
-uint8_t therms[] = THERMS;
+uint8_t therms[]  = THERMS;
 uint8_t heaters[] = HEATERS;
+uint8_t enables[] = ENABLES;
 
 unsigned long timers[NUM_TIMERS];
 
--- Slave/Globals.h
+++ Slave/Globals.h
@@ -12,8 +12,7 @@
 #define DEBUG_TEMP_TIMER 0
 
 extern Extruder extruders[];
-extern uint8_t therms[];
-extern uint8_t heaters[];
+extern uint8_t therms[], heaters[], enables[];
 
 extern unsigned long timers[];
 

288879f0ded424a4d2ab625f4c50ae3c5028d976
Patrick Bogen <pdbogen-github@cernu.us>
2015-05-01 17:33:29 -0700

    Commands for remote extruder enable/disable

--- Marlin/master_communications.cpp
+++ Marlin/master_communications.cpp
@@ -2,16 +2,36 @@
 #include "Communications.h"
 #include "temperature.h"
 
+void warn_bad_extruder( uint8_t extruder ) {
+	DEBUG_IO.print( "// cannot send command to set temp for remote extruder #" );
+	DEBUG_IO.println( extruder );
+}
+
 void store_extruder_temp_from_slave( const Payload &p ) {
 	current_temperature[1] = p.decimal;
 }
 
 void slave_set_extruder_temperature( uint8_t extruder, float target ) {
 	if( extruder == 0 ) {
-		output_queue.enqueue( SET_TEMP_0, Payload( target ) );
+		output_queue.enqueue( CMD_SET_TEMP_0, Payload( target ) );
 	} else {
-		DEBUG_IO.print( "// cannot send command to set temp for remote extruder #" );
-		DEBUG_IO.println( extruder );
+		warn_bad_extruder( extruder );
+	}
+}
+
+void slave_set_extruder_enabled( uint8_t extruder ) {
+	if( extruder == 0 ) {
+		output_queue.enqueue( CMD_SET_ENABLE_0 );
+	} else {
+		warn_bad_extruder( extruder );
+	}
+}
+
+void slave_set_extruder_disabled( uint8_t extruder ) {
+	if( extruder == 0 ) {
+		output_queue.enqueue( CMD_SET_DISABLE_0 );
+	} else {
+		warn_bad_extruder( extruder );
 	}
 }
 

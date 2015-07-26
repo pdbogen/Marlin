693df5aa88435c0c8e0d0306f49a351ab2cd69c7
Patrick Bogen <pdbogen-github@cernu.us>
2015-07-25 19:00:33 -0700

    Make selection of MarlinSerial contingent on _not_ using remote extruders

--- Marlin/Conditionals.h
+++ Marlin/Conditionals.h
@@ -214,7 +214,9 @@
 
   #include "pins.h"
 
-  #ifndef USBCON
+  // Master/Slave boards require HardwareSerial, so we can't disable it if we
+  // have remote extruders.
+  #if !defined(USBCON) && EXTRUDERS == LOCAL_EXTRUDERS
     #define HardwareSerial_h // trick to disable the standard HWserial
   #endif
 
